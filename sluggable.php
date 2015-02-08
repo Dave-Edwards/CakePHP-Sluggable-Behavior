bba2385e951902391467c62e5ad3866e052de370
Dave-Edwards <github@dave-edwards.com>
2015-02-08 10:23:50 +0000

    Make Behavior suitable for CakePHP 2.xx

--- sluggable.php
+++ sluggable.php
@@ -5,7 +5,7 @@
  *
  * Makes generation of unique URL slugs for permalinks very simple in CakePHP
  *
- * @version 1.1
+ * @version 2.0
  * @package CakePHP_Sluggable
  * @author Aaron Pollock <aaron.pollock@gmail.com>
  * @copyright Copyright (c) 2011-12, Aaron Pollock
@@ -17,8 +17,7 @@
  *
  * @package CakePHP_Sluggable
  */
-class SluggableBehavior extends ModelBehavior
-{
+class SluggableBehavior extends ModelBehavior {
 
 	/**
 	 * Settings as specified in the Cake model where the Behavior is applied
@@ -35,19 +34,19 @@ class SluggableBehavior extends ModelBehavior
 	 * @param mixed $model Instance of the model which has this Behavior
 	 * @return void
 	 */
-	public function setup(&$model, $settings)
+	public function setup(Model $model, $settings = array())
 	{
-		if (!isset($this->settings[$model->alias])) {
-			$this->settings[$model->alias] = array(
+		if (!isset($this -> settings[$model -> alias])) {
+			$this -> settings[$model -> alias] = array(
 				'slug_field'		=> 'slug',
 				'separator'		=> '-',
-				'title_field'		=> $model->displayField,
+				'title_field'		=> $model -> displayField,
                                 'update_existing'       => 'false'
 			);
 		}
 
-		$this->settings[$model->alias] = array_merge(
-			$this->settings[$model->alias],
+		$this -> settings[$model -> alias] = array_merge(
+			$this -> settings[$model -> alias],
 			(array)$settings
 		);
 	}
@@ -60,10 +59,10 @@ class SluggableBehavior extends ModelBehavior
 	 * @param mixed $model Instance of the model which has this Behavior
 	 * @return bool Always returns true, allowing CakePHP validation to proceed
 	 */
-	public function beforeValidate(&$model)
+	public function beforeValidate(Model $model, $options = array())
 	{
-		if (!$this->_slug_override_in_place($model) && $this->_record_needs_slug($model)){
-			$this->_generate_slug($model);
+		if (!$this -> _slug_override_in_place($model) && $this -> _record_needs_slug($model)){
+			$this -> _generate_slug($model);
 		}
 
 		return true;
@@ -77,10 +76,10 @@ class SluggableBehavior extends ModelBehavior
 	 * @param mixed $model Instance of the model which has this Behavior
 	 * @return bool True if data already contains slug
 	 */
-	private function _slug_override_in_place(&$model)
+	private function _slug_override_in_place(Model $model)
 	{
-		if (isset($model->data[$model->alias][$this->settings[$model->alias]['slug_field']])){
-			$slug_in_data = $model->data[$model->alias][$this->settings[$model->alias]['slug_field']];
+		if (isset($model -> data[$model -> alias][$this -> settings[$model -> alias]['slug_field']])){
+			$slug_in_data = $model -> data[$model -> alias][$this -> settings[$model -> alias]['slug_field']];
 		}
 
 		return (isset($slug_in_data) && !empty($slug_in_data));
@@ -92,9 +91,9 @@ class SluggableBehavior extends ModelBehavior
 	 * @param mixed $model Instance of the model which has this Behavior
 	 * @return bool True if a slug is required (false if one exists)
 	 */
-	private function _record_needs_slug(&$model)
+	private function _record_needs_slug(Model $model)
 	{
-		if (!isset($model->id)) {
+		if ($model -> id == FALSE) {
 
 			// new record, needs a slug in all cases
 			return true;
@@ -102,12 +101,12 @@ class SluggableBehavior extends ModelBehavior
 		} else {
 
 			// existing record; check to see if slug already present before generating one, or whether update_existing option has been set to overwrite an existing one on title change
-			$existing_data = $model->findById($model->id);
-			$existing_slug = $existing_data[$model->alias][$this->settings[$model->alias]['slug_field']];
-			$existing_title = $existing_data[$model->alias][$this->settings[$model->alias]['title_field']];
-                        $new_title = $model->data[$model->alias][$this->settings[$model->alias]['title_field']];
+			$existing_data = $model -> findById($model -> id);
+			$existing_slug = $existing_data[$model -> alias][$this -> settings[$model -> alias]['slug_field']];
+			$existing_title = $existing_data[$model -> alias][$this -> settings[$model -> alias]['title_field']];
+                        $new_title = $model->data[$model -> alias][$this -> settings[$model -> alias]['title_field']];
 
-			return ( null === $existing_slug || '' === $existing_slug || ($this->settings[$model->alias]['update_existing'] === 'true' && $existing_title != $new_title));
+			return ( null === $existing_slug || '' === $existing_slug || ($this->settings[$model -> alias]['update_existing'] === 'true' && $existing_title != $new_title));
 
 		}
 	}
@@ -118,31 +117,31 @@ class SluggableBehavior extends ModelBehavior
 	 * @param mixed $model Instance of the model which has this Behavior
 	 * @return mixed Nothing is returned
 	 */
-	private function _generate_slug(&$model)
+	private function _generate_slug(Model $model)
 	{
-		$title_field = $this->settings[$model->alias]['title_field'];
+		$title_field = $this -> settings[$model -> alias]['title_field'];
 
 		// use the record title as passed in the data being validated
-		if (isset($model->data[$model->alias][$title_field])) {
-			$slug_source = $model->data[$model->alias][$title_field];
+		if (isset($model -> data[$model -> alias][$title_field])) {
+			$slug_source = $model -> data[$model -> alias][$title_field];
 
 		// use the record title in the database
-		} elseif (isset($model->id) && $model->field($title_field)) {
-			$slug_source = $model->field($title_field);
+		} elseif (isset($model -> id) && $model -> field($title_field)) {
+			$slug_source = $model -> field($title_field);
 
 		}
 
 		if (isset($slug_source)) {
 
-			$slug = strtolower(Inflector::slug($slug_source, $this->settings[$model->alias]['separator']));
-			$max_length = $model->_schema[$this->settings[$model->alias]['slug_field']]['length'];
+			$slug = strtolower(Inflector::slug($slug_source, $this -> settings[$model -> alias]['separator']));
+			$max_length = $model -> _schema[$this -> settings[$model -> alias]['slug_field']]['length'];
 			if (strlen($slug) > $max_length) {
 				$slug = substr($slug, 0, $max_length);
 			}
 
-			$this->duplicate_suffix = 0;
-			$slug = $this->_deduplicate_slug($model, $slug);
-			$model->data[$model->alias][$this->settings[$model->alias]['slug_field']] = $slug;
+			$this -> duplicate_suffix = 0;
+			$slug = $this -> _deduplicate_slug($model, $slug);
+			$model->data[$model -> alias][$this -> settings[$model -> alias]['slug_field']] = $slug;
 
 		}
 	}
@@ -154,14 +153,14 @@ class SluggableBehavior extends ModelBehavior
 	 * @param string $slug The generated slug which needs checked for uniqueness and amended if required
 	 * @return string A final slug which has been found to be unique
 	 */
-	private function _deduplicate_slug(&$model, &$slug)
+	private function _deduplicate_slug(Model $model, &$slug)
 	{
-		$dupes = $model->find(
+		$dupes = $model -> find(
 			'count',
 			array(
 				'conditions' => array(
-					$model->alias . '.' . $this->settings[$model->alias]['slug_field'] => $slug,
-					$model->alias . '.id !=' => $model->id
+					$model -> alias . '.' . $this -> settings[$model -> alias]['slug_field'] => $slug,
+					$model -> alias . '.id !=' => $model -> id
 				)
 			)
 		);
@@ -172,13 +171,13 @@ class SluggableBehavior extends ModelBehavior
 
 		} else {
 
-			$previous_suffix = $this->duplicate_suffix;
-			$this->duplicate_suffix++;
+			$previous_suffix = $this -> duplicate_suffix;
+			$this -> duplicate_suffix++;
 
-			$new_slug_suffix = $this->settings[$model->alias]['separator'].(string)$this->duplicate_suffix;
+			$new_slug_suffix = $this -> settings[$model -> alias]['separator'].(string)$this -> duplicate_suffix;
 			$new_suffix_length = strlen($new_slug_suffix);
 			$slug_length = strlen($slug);
-			$max_length = $model->_schema[$this->settings[$model->alias]['slug_field']]['length'];
+			$max_length = $model -> _schema[$this -> settings[$model -> alias]['slug_field']]['length'];
 
 			if ($previous_suffix > 0 || $new_suffix_length + $slug_length > $max_length) {
 				$replace_at = -1 * strlen($previous_suffix) -1;
@@ -187,7 +186,7 @@ class SluggableBehavior extends ModelBehavior
 			}
 
 			$slug = substr_replace($slug, $new_slug_suffix, $replace_at);
-			return $this->_deduplicate_slug($model, $slug);
+			return $this -> _deduplicate_slug($model, $slug);
 
 		}
 	}
